b2f3db5426b9c7e9d5740ad01837459c932c3c24
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2020-12-22 11:49:55 +0000

    Make some more documentation improvements

--- app.py
+++ app.py
@@ -11,17 +11,25 @@ from werkzeug.exceptions import BadRequest
 app = Flask(__name__)
 
 # Load pre-trained model
-# Ref: Martin Thoma - https://stackoverflow.com/a/43263973
+# Reference: Martin Thoma - https://stackoverflow.com/a/43263973
 model = keras.models.load_model("power_prod.h5")
 
 
 @app.route("/")
 def home():
+    """Serves the HTML frontend."""
     return app.send_static_file("index.html")
 
 
 @app.route("/api/speed", methods=["POST"])
 def speed():
+    """
+    Takes a speed value via a POST request and returns the
+    predicted power output.
+
+    A floating point number called `query` should be included
+    in the body of the request.
+    """
     try:
         query = float(request.get_json()["query"])
 
